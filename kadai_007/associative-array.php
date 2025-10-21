cc6f105f0aa7adc27803d40a0836bdf4a3de900e
Riwa88 <like.a.box.of.chocolatee@gmail.com>
2025-10-21 12:28:39 +0900

    フォルダをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+<body>
+    <p>
+        <?php
+            $vegetable1 = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            print_r($vegetable1);
+        ?>
+    </p>
+</body>
+</html>
